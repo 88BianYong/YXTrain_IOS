e96c0768ef2d379f740cebab220bb29f2c26795c
zhengxiaolong <zhengxiaolong@yanxiu.com>
2017-07-14 11:17:18 +0800

    通知简报框架搭建

---
+++ TrainApp/Phone/UI/2017/NoticeBriefMangerViewController_17.m
@@ -0,0 +1,35 @@
+//
+//  NoticeBriefMangerViewController_17.m
+//  TrainApp
+//
+//  Created by 郑小龙 on 2017/7/14.
+//  Copyright © 2017年 niuzhaowang. All rights reserved.
+//
+
+#import "NoticeBriefMangerViewController_17.h"
+
+@interface NoticeBriefMangerViewController_17 ()
+
+@end
+
+@implementation NoticeBriefMangerViewController_17
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupUI];
+    [self setupLayout];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+#pragma mark - setupUI
+- (void)setupUI {
+    self.automaticallyAdjustsScrollViewInsets = NO;
+
+}
+- (void)setupLayout {
+    
+}
+@end
